663d87ce7377d193636a64586a5dc86a3433d22a
unknown <sabhyankar918@gmail.com>
2022-02-26 17:51:50 +0530

    added crew page

--- src/components/pages/Crew/Crew.js
+++ src/components/pages/Crew/Crew.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from "./Crew.module.scss"
+import { CrewData } from './Data'
+
 
 export default function Crew() {
+  const [crew, setCrew] = useState(CrewData[0])
+  
+
   return (
-    <div>Crew</div>
+    <section className={style.crew__container}>
+      <h2><span>02</span>Meet your crew</h2>
+      <div className={style.crew__inner_container}>
+        <div className={style.crew__image}>
+          <img src={crew.img} alt={crew.name} />
+        </div>
+        <div className={style.crew__content}>
+          <div className={style.crew__buttons}>
+            { CrewData.map( i => 
+            // console.log(index)
+              <span key={i.id} onClick={() => setCrew(i)}>
+              </span>
+            )}
+          </div>
+          <span>{crew.post}</span>
+          <h3>{crew.name}</h3>
+          <p>{crew.about}</p>
+        </div>
+      </div>
+
+    </section>
   )
 }
